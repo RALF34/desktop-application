71fa3167718af4e071a4db99b01519598e9a4616
RALF34 <raphaello34@hotmail.fr>
2023-12-10 18:11:53 +0100

    Add files via upload

--- daily_pollution.py
+++ daily_pollution.py
@@ -22,11 +22,11 @@ OMS_guidelines = {
     )
 }
 
-mongoClient = MongoClient()
+mongoClient = MongoClient(mongodb://localhost:8001)
 
 def get_choices_from_database(about=None, search_filter=None, key_0=None, key_1=None):
     if about == "regions":
-        items = mongoClient["LCSQA_stations"].distinct("_id.region")
+        items = mongoClient["LCSQA_stations"].find().distinct("_id.region")
     elif about == "stations" :
         items = mongoClient["LCSQA_stations"].find_one(
             search_filter
@@ -36,15 +36,13 @@ def get_choices_from_database(about=None, search_filter=None, key_0=None, key_1=
             lambda x: symbol_to_name[x],
             mongoClient["distribution_pollutants"].find_one(
             search_filter
-            )["monitored_pollutants"]
-        )
+            )["monitored_pollutants"])
     else:
         items = [
             e[key_1] for e in
             mongoClient["LCSQA_stations"].find(
                 search_filter
-            ).distinct(key_0)
-        ]
+            ).distinct(key_0)]
     return list(zip(range(1,len(items)+1),items))
 
 def get_input_from_user(about, choices, shorter_period=False):
@@ -62,8 +60,7 @@ def get_input_from_user(about, choices, shorter_period=False):
             séparer d'un espace.)"
         list_choices = [
             str(e[0])+" : "+"Pollution "+("à" if e[1]=="ozone" else "au")+e 
-            for e in choices
-        ]
+            for e in choices]
     elif not(shorter_period):
         message_to_user = "L'analyse de pollution est par défaut réalisée à \
             partir de valeurs de concentration atmosphérique\nobtenues sur les \
@@ -73,7 +70,7 @@ def get_input_from_user(about, choices, shorter_period=False):
         message_to_user = "Entrez le nombre de jours souhaité.\n"
         list_choices = [""]
     number = input(
-        message_to_user+"\n".join(list_choices)
+        message_to_user+"\n".join(list_choices))
     if about == "n_days" and not(shorter_period):
         if number:
             return "45"
@@ -92,53 +89,43 @@ if __name__ == "__main__":
     choices_regions = get_choices_from_database(about="regions")
     selected_number = int(get_input_from_user(
         "région",
-        choices_regions
-    ))
+        choices_regions))
     selected_region = choices_regions[selected_number-1][1]
 
     choices_departments = get_choices_from_database(
         search_filter={"_id": selected_region},
         key_0="departments",
-        key_1="department",
-    )
+        key_1="department")
     selected_number = int(get_input_from_user(
         "département",
-        choices_department
-    ))
+        choices_department))
     selected_department = choices_departments[selected_number-1][1]
-
+    
     choices_cities = get_choices_from_database(
         search_filter={"departments.department": selected_department},
         key_0="names",
-        key_1="city"
-    )
+        key_1="city")
     selected_number = int(get_input_from_user(
         "commune",
-        choices_cities
-    ))
+        choices_cities))
     selected_city = choices_cities[selected_number-1][1]
 
     choices_stations = get_choices_from_database(
         about="stations",
-        search_filter={"departments.names.city": selected_city}
-    )
+        search_filter={"departments.names.city": selected_city})
     selected_number = int(get_input_from_user(
         "station",
-        choices_stations
-    ))
+        choices_stations))
     s = choices_stations[selected_number-1][1]
 
     choices_pollutants = get_choices_from_database(
         about="pollutants",
-        search_filter={"_id": selected_station}
-    )
+        search_filter={"_id": selected_station})
     selected_numbers = list(map(
         lambda x: int(x),
-        get_input_from_user("pollutant",choices_pollutants).split()
-    ))
+        get_input_from_user("pollutant",choices_pollutants).split()))
     pollutants = [
-        name_to_symbol[choices_pollutants[i-1][1]]for i in selected_numbers
-    ]
+        name_to_symbol[choices_pollutants[i-1][1]]for i in selected_numbers]
 
     n = get_input_from_user("n_days",[])
 
@@ -151,13 +138,11 @@ if __name__ == "__main__":
     for i, p in enumerate(pollutants):
         ax.scatter(
             [x+"h00" for x in range(24)],
-            data[i].values()
-        )
+            data[i].values())
         max_value = max(data[i].values())
         thresholds = [
             (x/3)*OMS_guidelines[p]
-            for x in range(1,5)
-        ]
+            for x in range(1,5)]
         max_level = 2
         while (max_level < 5 and thresholds[max_level] < max_value):
             max_level += 1
@@ -174,8 +159,7 @@ if __name__ == "__main__":
                 thresholds[j],
                 y2=y_min,
                 color=colors[j],
-                alpha=0.1
-            )
+                alpha=0.1)
             y_min = thresholds[j]
             if j == 2:
                 ax.axhline(
@@ -184,20 +168,17 @@ if __name__ == "__main__":
                     linestyle="--",
                     linewidth=1.7,
                     label="Concentration \n moyenne \n"+ \
-                          "journalière \n recommandée (O.M.S)"
-                )
+                          "journalière \n recommandée (O.M.S)")
         if max_value > thresholds[max_level]:
             ax.fill_between(
                 list(range(24)),
                 ax.get_ylim()[1],
                 y2=thresholds[max_level],
                 color="magenta",
-                alpha=0.1
-            )
+                alpha=0.1)
         ax.set_yticks([0])
         ax.set_yticklabels([" "])
         ax.legend(loc="upper right")
         fig.savefig(
-            s+"/"+p+"("+str(n)+" jours)"
-        )
+            s+"/"+p+"("+str(n)+" jours)")
         fig.clf()
--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from fastapi import FastAPI, HTTPException, Path, Query
 from pandas import read_excel
 from pydantic import BaseModel, Field
 
-from .crud import get_response, initialize_database
-update_database
+from .crud import get_response, initialize_or_update_database
 
 app = FastAPI()
 
-initialize_database()
+initialize_or_update_database()
 
 class averageConcentrations(BaseModel):
     values: list[float] = Field(
@@ -29,20 +28,14 @@ async def get_response(
         Path(
             description=(
                 "Code identifying the air quality monitoring station \
-                 whose data will be used by the API."
-            ),
-            pattern="^FR([0-9]{5}$)"
-        )
-    ],
+                 whose data will be used by the API."),
+            pattern="^FR([0-9]{5}$)")],
     pollutants: Annotated[
         list[str],
         Query(
             description=(
                 "Pollutant(s) whose average daily variation of air \
-                 concentration(s) we want to display."
-            )
-        )
-    ],
+                 concentration(s) we want to display."))],
     n_days: Annotated[
         int,
         Query(
@@ -50,18 +43,10 @@ async def get_response(
                 "Parameter specifying the size (in number of days) of\
                  the period whose pollution data are being used by the\
                  API (i.e the analysis is based on data collected over\
-                 the last 'n_days' days)."
-            ),
+                 the last 'n_days' days)."),
             ge=1,
-            le=44
-        )
-    ]
-):
+            le=44)]):
     if station not in LCSQA_stations:
-        raise HTTPException(status_code=404,detail="Station not found")
+        raise HTTPException(status_code=404, detail="Station not found")
     else:
-        return crud.get_response(
-            station,
-            pollutants,
-            n_days
-        )
+        return crud.get_response(station, pollutants, n_days)
