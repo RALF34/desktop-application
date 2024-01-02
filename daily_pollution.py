85daeb81bd6be984ee3ff87d8c90418222020d51
RALF34 <raphaello34@hotmail.fr>
2024-01-02 10:54:01 +0100

    Add files via upload

--- daily_pollution.py
+++ daily_pollution.py
@@ -1,8 +1,20 @@
 import json
-import requests
+import re
+import subprocess
+import time
 
+import requests
 from matplotlib import pyplot
 from pymongo import MongoClient
+
+overseas_departments = [
+    "GUADELOUPE",
+    "GUYANE",
+    "MARTINIQUE",
+    "LA REUNION",
+    "MAYOTTE",
+    "SAINT-MARTIN"
+]
 
 symbol_to_name = {
     "O3": "ozone",
@@ -15,6 +27,7 @@ symbol_to_name = {
 name_to_symbol = {
     v: k for (k, v) in symbol_to_name.items()
 }
+
 OMS_guidelines = {
     pollutant: value for (pollutant, value) in zip(
         symbol_to_name.keys(),
@@ -22,115 +35,145 @@ OMS_guidelines = {
     )
 }
 
-mongoClient = MongoClient(mongodb://localhost:8001)
+mongoClient = MongoClient() # mongodb://localhost:8001
+database = mongoClient["air_quality"]
 
-def get_choices_from_database(about=None, search_filter=None, key_0=None, key_1=None):
-    if about == "regions":
-        items = mongoClient["LCSQA_stations"].find().distinct("_id.region")
-    elif about == "stations" :
-        items = mongoClient["LCSQA_stations"].find_one(
-            search_filter
-        )["names"]
-    elif about=="pollutants":
-        items = map(
-            lambda x: symbol_to_name[x],
-            mongoClient["distribution_pollutants"].find_one(
-            search_filter
-            )["monitored_pollutants"])
-    else:
-        items = [
-            e[key_1] for e in
-            mongoClient["LCSQA_stations"].find(
+def get_choices_from_database(about, search_filter=None):
+    match about:
+        case "regions":
+            items = database["regions"].find().distinct("_id")
+            for e in overseas_departments:
+                items.remove(e)
+        case "departments":
+            if search_filter["_id"] == "OUTRE-MER":
+                items = overseas_departments
+            else:
+                items = list(set(database["regions"].find_one(
+                    search_filter)["departments"]))
+        case "cities":
+            items = list(set(database["departments"].find_one(
                 search_filter
-            ).distinct(key_0)]
-    return list(zip(range(1,len(items)+1),items))
+            )["cities"]))
+        case "stations":
+            list_of_stations = database["cities"].find_one(
+                search_filter
+            )["stations"]
+            items = list(set([e["name"]+" (zone "+e["zone"]+")"+e["code"] 
+            for e in list_of_stations]))
+        case "pollutants":
+            items = list(map(
+                lambda x: symbol_to_name[x],
+                list(set(database["distribution_pollutants"].find_one(
+                    search_filter
+                )["monitored_pollutants"]))))
+    choices = list(zip(range(1,len(items)+1), sorted(items)))
+    if about == "regions":
+        choices.append((len(choices)+1,"OUTRE MER"))
+    return choices
 
 def get_input_from_user(about, choices, shorter_period=False):
-    if len(choices) == 1:
-        return "0"
-    elif (about in ["région","département","commune","station"]):
+    french_translation = {
+        "regions": "régions",
+        "departments": "départements",
+        "cities": "communes",
+        "stations": "stations"}
+    if about in french_translation.keys():
         message_to_user = "Veuillez indiquer le numéro correspondant "+\
-            ("à la " if about in ["région","commune","station"] else "au ")+\
-            about + "de votre choix\n"+(" (puis tapez 'Entrée' pour continuer)."
-            if about == "région" else ".")
-        list_choices = [str(e[0])+" : "+e[1] for e in choices]
-    elif about == "pollutant":
-        message_to_user = "Par quelle type de pollution ètes-vous intéressé ? \n\
-            (Pour indiquer plusieurs choix, entrez les numéros\n\correspondants \
-            séparer d'un espace.)"
+            ("à la " if about in ["regions","cities","stations"] else "au ")+\
+            french_translation[about][:-1] + " de votre choix."+"\n\n"
         list_choices = [
-            str(e[0])+" : "+"Pollution "+("à" if e[1]=="ozone" else "au")+e 
+            str(e[0])+" : "+(e[1] if not(about=="stations") 
+            else e[1][:e[1].index(")")+1]) for e in choices]
+    elif about == "pollutants":
+        message_to_user = "Par quelle type de pollution ètes-vous intéressé ?\n\n"
+        list_choices = [
+            str(e[0])+" : Pollution "+("à" if e[1]=="ozone" else "au")+e 
             for e in choices]
     elif not(shorter_period):
         message_to_user = "L'analyse de pollution est par défaut réalisée à \
             partir de valeurs de concentration atmosphérique\nobtenues sur les \
-            45 derniers jours. Voulez-vous considérer une période plus courte ?"
+            45 derniers jours. Voulez-vous considérer une période plus courte ?\n\n"
         list_choices = ["Si oui, entrez 0.", "Sinon, entrez 1."]
     else:
-        message_to_user = "Entrez le nombre de jours souhaité.\n"
+        message_to_user = "Entrez le nombre de jours souhaité.\n\
+            (Indiquez '0' pour revenir aux choix précédents)\n\n"
         list_choices = [""]
-    number = input(
-        message_to_user+"\n".join(list_choices))
+    if about != "regions":
+        list_choices += ["\n"+str(len(list_choices)+1)+ " : Retour aux choix précédents"]
+    
+    text =  message_to_user+"\n".join(list_choices)+"\n"
+    number = input(text)
+    x = 0 if about == "n_days" and not(shorter_period) else 1
+    y = 45 if about == "n_days" and shorter_period else len(list_choices)+1
+    while not (number.isdigit() and int(number) not in range(x,y)):
+        number = input(text)
     if about == "n_days" and not(shorter_period):
-        if number:
+        if int(number):
             return "45"
         else:
             return get_input_from_user(about, choices, shorter_period=True)
     else:
-        if not(shorter_period):
-            while int(number) not in range(1,len(choices)+1):
-                number = get_input_from_user(about, choices)
-        else:
-            while int(number) not in range(1,45):
-                number = get_input_from_user(about, choices, shorter_period=True)
         return number
 
-if __name__ == "__main__":
-    choices_regions = get_choices_from_database(about="regions")
-    selected_number = int(get_input_from_user(
-        "région",
-        choices_regions))
-    selected_region = choices_regions[selected_number-1][1]
+def get_selected_item(about, filter_item=None):
+    if filter_item is None:
+        choices = get_choices_from_database(about)
+    else:
+        choices = get_choices_from_database(about, search_filter={"_id": filter_item})
+    selected_number = int(get_input_from_user(about, choices))
+    item = 0 if selected_number == len(choices) else choices[selected_number-1][1]
+    return item
 
-    choices_departments = get_choices_from_database(
-        search_filter={"_id": selected_region},
-        key_0="departments",
-        key_1="department")
-    selected_number = int(get_input_from_user(
-        "département",
-        choices_department))
-    selected_department = choices_departments[selected_number-1][1]
+def get_query_parameters(about, item=None, previous_item=None):
+    match about:
+        case "regions":
+            region = get_selected_item(about)
+            return get_query_parameters("departments", item=region)
+        case "departments":
+            department = get_selected_item(about, item)
+            if type(department) is int:
+                return get_query_parameters("regions")
+            else:
+                return get_query_parameters("cities", item=department, previous_item=item)
+        case "cities":
+            city = get_selected_item(about, item)
+            if type(city) is int:
+                return get_query_parameters("departments", item=previous_item)
+            else:
+                return get_query_parameters("stations", item=city, previous_item=item)
+        case "stations":
+            s = get_selected_item(about, item)
+            if type(s) is int:
+                return get_query_parameters("cities", item=previous_item)
+            else:
+                station, code = s[:s.index("(")-1], s[s.index(")")+1:]
+                return code, get_query_parameters("pollutants", item=station, previous_item=item)
+        case "pollutants":
+            pollutant = name_to_symbol[get_selected_item("pollutants", item)]
+            if type(pollutant) is int:
+                return get_query_parameters("stations", item=previous_item)
+            else:
+                return pollutant, get_query_parameters("n_days", previous_item=item)
+        case "n_days":
+            n_days = get_input_from_user("n_days", [])
+            if not(n_days):
+                return get_query_parameters("pollutants", item=previous_item)
+            else:
+                return n_days
+
+if __name__=="__main__":
+    i = 0
+    while "last_update" not in database.list_collection_names():
+        if i == 4:
+            i = 0
+        print("Initialisation des données en cours"+"."*i+"\r")
+        i += 1
+        time.sleep(0.7)
     
-    choices_cities = get_choices_from_database(
-        search_filter={"departments.department": selected_department},
-        key_0="names",
-        key_1="city")
-    selected_number = int(get_input_from_user(
-        "commune",
-        choices_cities))
-    selected_city = choices_cities[selected_number-1][1]
-
-    choices_stations = get_choices_from_database(
-        about="stations",
-        search_filter={"departments.names.city": selected_city})
-    selected_number = int(get_input_from_user(
-        "station",
-        choices_stations))
-    s = choices_stations[selected_number-1][1]
-
-    choices_pollutants = get_choices_from_database(
-        about="pollutants",
-        search_filter={"_id": selected_station})
-    selected_numbers = list(map(
-        lambda x: int(x),
-        get_input_from_user("pollutant",choices_pollutants).split()))
-    pollutants = [
-        name_to_symbol[choices_pollutants[i-1][1]]for i in selected_numbers]
-
-    n = get_input_from_user("n_days",[])
-
-    data = json.loads(request.get(
-        "http://127.0.0.1/?station=s&pollutants=pollutants&n_days=n"
+    code, pollutant, n_days = get_query_parameters("regions")
+    
+    data = json.loads(requests.get(
+        "http://127.0.0.1:8000/?s=code&p=pollutants&n=n_days"
     ).json())
 
     fig, ax = pyplot.subplots()
@@ -179,6 +222,4 @@ if __name__ == "__main__":
         ax.set_yticks([0])
         ax.set_yticklabels([" "])
         ax.legend(loc="upper right")
-        fig.savefig(
-            s+"/"+p+"("+str(n)+" jours)")
-        fig.clf()
+        pyplot.show()
